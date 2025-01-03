3c07a0fbc195e00f6a220ec0e7552d9401a192f2
Yaswanth-1822 <yaswanthkumarpudi623@gmail.com>
2025-01-03 18:36:46 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }
+});
+
+// Create a User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
